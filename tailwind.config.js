62c31426591b0c7804f057583be56888865e3328
Rendy Tomaluweng <workspace.rendy@gmail.com>
2024-08-20 23:16:03 +0700

    footer onprocess

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,12 @@ module.exports = {
     boxShadow: {
       shadowButton: "box-shadow: 47px 78px 149px 0px rgba(0,0,0,0.36)"
     },
+    keyframes: {
+      move: "0% {right: -100%;} 100% {left: 100%;}"
+    },
   },
-  extend: {},
+  extend: {
+  },
   plugins: [],
 }
 
