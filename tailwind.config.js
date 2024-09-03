5cfa40fbe3212d1c35cf41eca5f1c68638f7d123
Rendy Tomaluweng <workspace.rendy@gmail.com>
2024-09-03 23:35:08 +0700

    beranda responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,12 @@ module.exports = {
             "./component/**/*.{png,jpeg,jpg,svg}",
             "./component/**/*.{html,js}",
             "./index.html"],
+
   theme: {
     fontFamily: {
       primary: ["Overpass", "sans-serif"]
     },
+
     extend: {
       keyframes: {
         'slide-left': {
@@ -15,21 +17,24 @@ module.exports = {
           '100%': {transform: 'translateX(-1%)'},
         }
       },
+
       animation: {
         'slide-left': 'slide-left 5s linear infinite',
       },
+
       colors: {
         'mypink': '#FB6F92',
       },
+
       screens: {
-        'mb': {'min': '320px', 'max': '768px'},
-        'tb': {'min': '768px', 'max': '1024px'},
-        'de': {'min': '1025px', 'max': '1280px'}
+        'sm2x': {'min': '0px', 'max': '639px'},
       }
     }
   },
+
   extend: {
   },
+
   plugins: [],
 }
 
